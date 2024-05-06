26528d6c52b85b2a1e3f0a229c24943bd86b352f
Duncan Miller <duncan@openshiro.com>
2024-05-05 21:57:29 -0700

    modify stimulus controller to close menu after links are clicked

--- frontend/javascript/controllers/toggle_controller.js
+++ frontend/javascript/controllers/toggle_controller.js
@@ -1,8 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
-  static targets = ["menu", "button", "closeButton"];
+  static targets = ["menu", "button", "closeButton", "link"];
 
   toggleMenu() {
     this.menuTarget.classList.toggle('hidden');
+  }
+
+  closeMenu() {
+    this.menuTarget.classList.add('hidden');
   }
 }
