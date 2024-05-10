5aaf8be1dec976e17628c02356d9b9f7182e0890
Duncan Miller <duncan@openshiro.com>
2024-05-10 16:17:58 -0700

    reorganize pages

--- test/test_homepage.rb
+++ test/test_homepage.rb
@@ -3,7 +3,7 @@ require_relative "./helper"
 class TestHomepage < Minitest::Test
   context "homepage" do
     setup do
-      page = site.collections.pages.resources.find { |doc| doc.relative_url == "/" }
+      page = site.collections.pages.resources.find { |doc| doc.relative_url == "/home/" }
       document_root page
     end
 
