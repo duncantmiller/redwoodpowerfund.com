8393914aae4207bad28fb232f6eb010dc2f60e44
Duncan Miller <duncan@openshiro.com>
2024-10-06 22:16:06 -0700

    fixing tests for pagination

--- test/test_blog.rb
+++ test/test_blog.rb
@@ -3,28 +3,48 @@ require_relative "helper"
 class TestBlog < Minitest::Test
   context "blog" do
     setup do
-      page = site.collections.pages.resources.find { |page| page.relative_url == "/blog/" }
-      document_root page
+      @site = Bridgetown::Site.new(Bridgetown.configuration)
+      @site.process
+
+      @pages = @site.generated_pages.select { |page| page.relative_url.start_with?("/blog") }
+      # Optionally, test only the first page or iterate through all pages
+      document_root @pages.first  # This will select the first page (or adjust if testing paginated pages)
     end
 
     should "exist" do
-      assert_select "body"
+      assert_select "body", true, "Page should exist with a <body> element"
     end
 
     should "display blog titles" do
-      assert_select "h3 a", true, "Blog titles should be present and contained within <h3> tags"
+      @pages.each do |page|
+        document_root page
+        assert_select "h3 a", true, "Blog titles should be present and contained within <h3> tags"
+      end
     end
 
     should "display author names" do
-      assert_select ".text-sm a", true, "Author names should be present and contained within the specified selector"
+      @pages.each do |page|
+        document_root page
+        assert_select ".text-sm a", true, "Author names should be present and contained within the specified selector"
+      end
     end
 
     should "include post images for each blog entry" do
-      assert_select "article a img", true, "Each blog entry should have at least one post image within an anchor tag"
+      @pages.each do |page|
+        document_root page
+        assert_select "article a img", true, "Each blog entry should have at least one post image within an anchor tag"
+      end
     end
 
     should "include author images for each blog entry" do
-      assert_select "article img.rounded-full", true, "Each blog entry should have at least one author image with 'rounded-full' class"
+      @pages.each do |page|
+        document_root page
+        assert_select "article img.rounded-full", true, "Each blog entry should have at least one author image with 'rounded-full' class"
+      end
+    end
+
+    should "include pagination links" do
+      assert_select "a", /\d+/, "Page numbers should be displayed"
     end
   end
 end
