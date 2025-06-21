a0c3d9c3b215ea8c1362c36a0e823891e048de67
Duncan Miller <1707425+duncantmiller@users.noreply.github.com>
2025-06-21 16:15:27 -0700

    fixing componenets for new layout

---
+++ src/_components/footer.rb
@@ -0,0 +1,5 @@
+class Footer < Bridgetown::Component
+  def initialize(**options)
+    @options = options
+  end
+end
--- src/_components/header.rb
+++ src/_components/header.rb
@@ -1,12 +1,13 @@
 class Header < Bridgetown::Component
-  def initialize(**options)
+  def initialize(is_home: false, **options)
+    @is_home = is_home
     @options = options
   end
 
   private
 
   def is_home_page?
-    resource&.url == "/"
+    @is_home
   end
 
   def navigation_items
