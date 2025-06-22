d5377ced9f13014d7a17168a015002526ca9dccf
Duncan Miller <1707425+duncantmiller@users.noreply.github.com>
2025-06-21 17:18:14 -0700

    page titles

--- src/_components/footer.rb
+++ src/_components/footer.rb
@@ -2,4 +2,16 @@ class Footer < Bridgetown::Component
   def initialize(**options)
     @options = options
   end
+
+  private
+
+  def student_name
+    # Try to access site data through Bridgetown's current site
+    site_instance = Bridgetown::Current.site
+    if site_instance&.data&.dig("personal_info", "name")
+      site_instance.data.dig("personal_info", "name")
+    else
+      "Rose City Robotics"
+    end
+  end
 end
